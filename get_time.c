fcf59f39cc51302fabf5af9a415969510b479a37
Sakis Panou <sakis.panou@xilinx.com>
2019-12-19 13:15:40 -0800

    Cleaning Up printTable() and reformating code.
    Signed-off-by: Sakis Panou <sakis.panou@xilinx.com>

--- get_time.c
+++ get_time.c
@@ -9,7 +9,10 @@ using namespace std;
 unsigned long timeInSeconds(struct tm time, bool adjustForUTC);
 bool isYearLeap(unsigned long year);
 bool leapYearChecker(unsigned int start, unsigned int end);
-void printTable(unsigned int table[],unsigned int size);
+
+void printTable(const unsigned int* table, 
+	const unsigned int tableSize, 
+	const unsigned char rowLength);
 
 const static unsigned long epochYear = 1970;
 const static unsigned long secondsInYear = 31536000;
@@ -22,154 +25,22 @@ const static unsigned long yearInStructTm = 1900;
 // https://kalender-365.de/leap-years.php
 // https://miniwebtool.com/leap-years-list/?start_year=1800&end_year=2400
 unsigned int leapYearCheckTable[] = {
-	1804,
-	1808,
-	1812,
-	1816,
-	1820,
-	1824,
-	1828,
-	1832,
-	1836,
-	1840,
-	1844,
-	1848,
-	1852,
-	1856,
-	1860,
-	1864,
-	1868,
-	1872,
-	1876,
-	1880,
-	1884,
-	1888,
-	1892,
-	1896,
-	1904,
-	1908,
-	1912,
-	1916,
-	1920,
-	1924,
-	1928,
-	1932,
-	1936,
-	1940,
-	1944,
-	1948,
-	1952,
-	1956,
-	1960,
-	1964,
-	1968,
-	1972,
-	1976,
-	1980,
-	1984,
-	1988,
-	1992,
-	1996,
-	2000,
-	2004,
-	2008,
-	2012,
-	2016,
-	2020,
-	2024,
-	2028,
-	2032,
-	2036,
-	2040,
-	2044,
-	2048,
-	2052,
-	2056,
-	2060,
-	2064,
-	2068,
-	2072,
-	2076,
-	2080,
-	2084,
-	2088,
-	2092,
-	2096,
-	2104,
-	2108,
-	2112,
-	2116,
-	2120,
-	2124,
-	2128,
-	2132,
-	2136,
-	2140,
-	2144,
-	2148,
-	2152,
-	2156,
-	2160,
-	2164,
-	2168,
-	2172,
-	2176,
-	2180,
-	2184,
-	2188,
-	2192,
-	2196,
-	2204,
-	2208,
-	2212,
-	2216,
-	2220,
-	2224,
-	2228,
-	2232,
-	2236,
-	2240,
-	2244,
-	2248,
-	2252,
-	2256,
-	2260,
-	2264,
-	2268,
-	2272,
-	2276,
-	2280,
-	2284,
-	2288,
-	2292,
-	2296,
-	2304,
-	2308,
-	2312,
-	2316,
-	2320,
-	2324,
-	2328,
-	2332,
-	2336,
-	2340,
-	2344,
-	2348,
-	2352,
-	2356,
-	2360,
-	2364,
-	2368,
-	2372,
-	2376,
-	2380,
-	2384,
-	2388,
-	2392,
-	2396,
-	2400,
+	1804, 1808, 1812, 1816, 1820, 1824, 1828, 1832, 1836, 1840,
+	1844, 1848, 1852, 1856, 1860, 1864, 1868, 1872, 1876, 1880,
+	1884, 1888, 1892, 1896, 1904, 1908, 1912, 1916, 1920, 1924,
+	1928, 1932, 1936, 1940, 1944, 1948, 1952, 1956, 1960, 1964,
+	1968, 1972, 1976, 1980, 1984, 1988, 1992, 1996, 2000, 2004,
+	2008, 2012, 2016, 2020, 2024, 2028, 2032, 2036, 2040, 2044,
+	2048, 2052, 2056, 2060, 2064, 2068, 2072, 2076, 2080, 2084,
+	2088, 2092, 2096, 2104, 2108, 2112, 2116, 2120, 2124, 2128,
+	2132, 2136, 2140, 2144, 2148, 2152, 2156, 2160, 2164, 2168,
+	2172, 2176, 2180, 2184, 2188, 2192, 2196, 2204, 2208, 2212,
+	2216, 2220, 2224, 2228, 2232, 2236, 2240, 2244, 2248, 2252,
+	2256, 2260, 2264, 2268, 2272, 2276, 2280, 2284, 2288, 2292,
+	2296, 2304, 2308, 2312, 2316, 2320, 2324, 2328, 2332, 2336,
+	2340, 2344, 2348, 2352, 2356, 2360, 2364, 2368, 2372, 2376,
+	2380, 2384, 2388, 2392, 2396, 2400
 };
-
 
 int main(int argc, char* argv[]){
 
@@ -292,7 +163,7 @@ bool leapYearChecker(unsigned int start, unsigned int end){
 	cout << endl;
 	cout << "========== Printing Golden Table ==========" << endl;
 	cout << "Table Size = "  << leapYearTableSize << endl;
-	printTable(leapYearCheckTable, leapYearTableSize);
+	printTable(leapYearCheckTable, leapYearTableSize, 10);
 	cout << endl;
 
 	for(unsigned int i = start; i <= end; i++){
@@ -304,7 +175,7 @@ bool leapYearChecker(unsigned int start, unsigned int end){
 	cout << endl;
 	cout << "========== Printing Calculated Table =========="  << endl;
 	cout << "Table Size = "  << calculatedLeapYearTableIndex << endl;
-	printTable(calculatedLeapYearTable, calculatedLeapYearTableIndex);
+	printTable(calculatedLeapYearTable, calculatedLeapYearTableIndex, 10);
 	cout << endl;
 
 	if(leapYearTableSize != calculatedLeapYearTableIndex){
@@ -320,23 +191,28 @@ bool leapYearChecker(unsigned int start, unsigned int end){
 	return(ret);
 }
 
-void printTable(unsigned int table[],unsigned int size){
+//=============================================================================
+//  
+// printTable: Prints a table of data in tabular format
+//
+// Prints a table of data in tabular format taking in as parameters 
+// the table itself, the size of the table, and the row length.  
+// 
+// 
+// Notes: N/A 
+//=============================================================================
+void printTable(const unsigned int* table, const unsigned int size, 
+	const unsigned char blockSize){
 
-	const unsigned int blockSize = 8;
+	if((0==table) || (0==size) || (0==blockSize))
+		return;
 
-	for(unsigned int i = 0; i < size; i++){
-
-		if((0 == (i % blockSize)) && (0!=i)) {
-			if(i != blockSize)
-				cout << table[i] << ", " << endl;
+	for(unsigned int i = 0; i < size; i++) {
+		if(0 == ((i+1) % (blockSize))) {
+			cout << table[i]  << "," << endl;
 		} else {
-			
-			cout << table[i] << ", ";
-
-			if(i == (blockSize -1))
-			 	cout << endl;
+			cout << table[i]  << ", " ; 
 		}
 	}
-
 	cout << endl;
 }
