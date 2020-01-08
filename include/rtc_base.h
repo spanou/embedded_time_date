43b641dd0320740417bbf9f1a79c58d642afe83f
Sakis Panou <sakis.panou@xilinx.com>
2020-01-08 11:19:46 -0800

    Minor Comments Added
    Signed-off-by: Sakis Panou <sakis.panou@xilinx.com>

--- include/rtc_base.h
+++ include/rtc_base.h
@@ -1,7 +1,10 @@
 #ifndef __RTC_BASE_H__
 #define __RTC_BASE_H__
 
-
+/**
+ * [gcc -dM -E - < /dev/null] prints all the predefined macros
+ * if -ansi or -std=c90 inline is not defined but __inline__ is.
+ */
 #ifdef __STDC_VERSION__
 	#if(__STDC_VERSION__ >= 199901L)
 		#define INLINE inline
